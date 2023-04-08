199cb4cdf2d29d1d3a8e63e77502e2e250d972b7
Joalor64 <rhgarcon@gmail.com>
2023-04-08 13:29:37 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -86,7 +86,7 @@ class Main extends Sprite
 		SUtil.check();
 	
 		ClientPrefs.loadDefaultKeys();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
